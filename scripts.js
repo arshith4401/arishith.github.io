b09282dea61ac02b920c5d0f291b5e0cc4da694a
Arishith Pakalapati <arishithpakalapati@arishiths-air.mynetworksettings.com>
2024-05-25 02:06:19 -0400

    updated

---
+++ scripts.js
@@ -0,0 +1,10 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('main section');
+    sections.forEach(section => {
+        if (section.id === sectionId) {
+            section.classList.remove('hidden');
+        } else {
+            section.classList.add('hidden');
+        }
+    });
+}
